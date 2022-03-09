e9543db7fcd612103df4f14ffe7744d69c2f5a72
chinmay116 <72141633+chinmay116@users.noreply.github.com>
2022-03-09 14:11:18 +0530

    Update Second.js

--- src/Components/Second.js
+++ src/Components/Second.js
@@ -1,5 +1,8 @@
 import React from 'react'
+import Shop from '../Assets/shop.png'
 import SearchIcon from '@mui/icons-material/Search';
+import VisibilityOutlinedIcon from '@mui/icons-material/VisibilityOutlined';
+import ExploreOutlinedIcon from '@mui/icons-material/ExploreOutlined';
 
 function Second() {
   return (
@@ -19,30 +22,38 @@ function Second() {
       </div>
 
       {/* Welcome user */}
-      <div>
+      <div className='mt-5'>
         <h1 className='text-2xl font-medium'>Hello David</h1>
-        <h3 className='text-normal'>Welcome Back!</h3>
+        <h3 className='text-normal text-gray-500'>Welcome Back!</h3>
       </div>
 
 
       {/* Pahila dabba */}
-      <div className='bg-yellow-200 rounded-lg'>
-        <div>
-          <h1>Views</h1>
-        </div>
-        <div>
-          <h1>Visits</h1>
+      <div className='bg-yellow-100 rounded-lg mt-5 p'>
+        <div className='flex justify-center items-center space-x-20'>
+          <div>
+            <VisibilityOutlinedIcon/>
+            <h1 className='text-gray-500'>Views</h1>
+            <h3 className='text-2xl font-medium'>500</h3>
+            <h3>Per Day</h3>
+          </div>
 
-        </div>
-        <div>
-          <h1>Orders</h1>
+          <div className='justify-center items-center'>
+            <ExploreOutlinedIcon/>
+            <h1 className='text-gray-500'>Visits</h1>
+            <h3 className='text-2xl font-medium'>2000</h3>
+            <h3>Per Minutes</h3>
+          </div>
 
+          <div>
+            <img className='w-8 h-8 mr-5' src={Shop} alt="Shop Icon" />
+            <h1 className='text-gray-500'>Orders</h1>
+            <h3 className='text-2xl font-medium'>5100</h3>
+            <h3>Per Day</h3>
+          </div>
         </div>
       </div>
-
     </div>
-
-
   )
 }
 
