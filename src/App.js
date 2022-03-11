db71932bad417c570ce50cec9f8825d502c9bb98
rohan9024 <64919642+rohan9024@users.noreply.github.com>
2022-03-11 17:56:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import Third from './Components/Third';
 
 function App() {
   return (
-    <div className="flex">
+    <div className="flex bg-gray-100">
       <First />
-      <div className='ml-10 w-[1px] h-screen bg-gray-200 drop-shadow-md' />
+      <div className='ml-10 w-[1px] h-screen bg-gray-200 drop-shadow-sm' />
 
 
       <Second />
