9cd357209aba7f0dca0576cd5cbff4cc8fe51bec
rohan9024 <64919642+rohan9024@users.noreply.github.com>
2022-03-11 17:56:13 +0530

    Update Second.js

--- src/Components/Second.js
+++ src/Components/Second.js
@@ -7,25 +7,52 @@ function Second() {
 
   const [feedsData, setFeedsData] = useState({});
   const [token, setToken] = useState('');
-  const [obj, setObj] = useState("")
-  const [followers, setFollowers] = useState("")
+  const [obj, setObj] = useState()
+  const [followers, setFollowers] = useState()
+  // const [posts, setPosts] = useState()
 
+  //   {
+  //     "business_discovery": {
+  //        "followers_count": 1,
+  //        "media_count": 2,
+  //        "media": {
+  //           "data": [
+  //              {
+  //                 "comments_count": 1,
+  //                 "like_count": 2,
+  //                 "id": "17917228841192422"
+  //              },
+  //              {
+  //                 "comments_count": 1,
+  //                 "like_count": 2,
+  //                 "id": "17951548414706253"
+  //              }
+  //           ]
+  //        },
+  //        "id": "17841452131317817"
+  //     },
+  //     "id": "17841452131317817"
+  //  }
+
+
+  // function fetchData() {
+  //   fetch(`https://graph.facebook.com/v3.2/17841452131317817?fields=business_discovery.username(_aaila_aalia_){followers_count,media_count,media{comments_count,like_count}}&access_token=EAANFg0BMulMBADqQaDJGzZAC5Bmf5BQBLHKhFBDoZADXJLK1gGCbJ79Tw9sjilS8zfKPg2CTKsuEdR3q2wrAjoVqJOsee8l1rsscsVI1sTTxIRDCuHhnOCyl2kYJpZCuitTTnSqMeVaAfiL7YB01fYY7OrZANidVDnbn5alYvHPiyaLiEEnH`)
+  //     .then(response => {
+  //       return response.json()
+  //     })
+  //     .then(data => {
+  //       setFeedsData(data)
+  //     })
+  //   console.log(feedsData)
+  //   const str = JSON.stringify(feedsData);
+  //   setObj(JSON.parse(str));
+  //   console.log(obj)
+  //   setFollowers(str.business_discovery.followers_count);
+  //   console.log(followers)
+
+  // }
 
   // useEffect(() => {
-  //   async function fetchData() {
-  //     const res = await fetch(`https://graph.facebook.com/v3.2/17841415660831513?fields=business_discovery.username(_is.it_raghav){followers_count,media_count}&access_token=EABCgQOxqTP8BADykWHQlubLRQtlLd6tYSncCRWZADPufbse7ombSZC8cLYFQdhp9Boxb0ZBUXKD5wSnTWedZBKTRvBZA5VsbSlOabNsbCmN3qOpkZBDUgZBd2Arju4bfYxNmhWPB3xB0RB16MZBQc3O0nliimTBoUn2S1mfSZA7kKZAwZDZD`);
-  //     res
-  //       .json()
-  //       .then(res => setFeedsData(res))
-  //       .catch((err) => console.log(err));
-
-  //       // console.log(res)f
-  //     const str = JSON.stringify(feedsData);
-  //     setObj(JSON.parse(str));
-  //     console.log(obj)
-  //     // setFollowers(obj.business_discovery.followers_count);
-  //     // console.log(followers)
-  //   }
   //   fetchData()
   // }, [])
 
@@ -42,26 +69,26 @@ function Second() {
 
 
       {/* Pahila dabba */}
-      <div className=' bg-yellow-100 rounded-lg mt-5 w-[800px] h-[300px] flex justify-evenly items-center space-x-12'>
+      <div className=' bg-red-100 rounded-lg mt-5 w-[800px] h-[300px] flex justify-evenly items-center space-x-12'>
         <div>
-          <div className='flex justify-center items-center w-16 h-16 bg-yellow-500 rounded-full'>
-            <img className='w-10 h-10' src={FOLLOWERS} alt="followers" />
+          <div className='flex justify-center items-center w-16 h-16 bg-red-500 rounded-full'>
+            <img className='w-6 h-6' src={FOLLOWERS} alt="followers" />
           </div>
           <h1 className='text-normal mt-6 ml-1 text-gray-600'>Followers</h1>
           <h3 className='text-4xl font-bold mt-10 ml-1'>1M</h3>
         </div>
 
         <div>
-          <div className='flex justify-center items-center w-16 h-16 bg-yellow-500 rounded-full'>
-            <img className='w-10 h-10 ' src={following} alt="following" />
+          <div className='flex justify-center items-center w-16 h-16 bg-red-500 rounded-full'>
+            <img className='w-6 h-6 ' src={following} alt="following" />
           </div>
           <h1 className='text-normal mt-6 ml-1 text-gray-600'>Following</h1>
           <h3 className='text-4xl font-bold mt-10 ml-1'>100</h3>
         </div>
 
         <div>
-          <div className='flex justify-center items-center w-16 h-16 bg-yellow-500 rounded-full'>
-            <img className='w-10 h-10' src={posts} alt="posts" />
+          <div className='flex justify-center items-center w-16 h-16 bg-red-500 rounded-full'>
+            <img className='w-6 h-6' src={posts} alt="posts" />
           </div>
           <h1 className='text-normal mt-6 ml-4 text-gray-600'>Posts</h1>
           <h3 className='text-4xl font-bold mt-10 '>2000</h3>
